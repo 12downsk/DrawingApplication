08c74040e1e05728f4689e7507d63796f43f007b
12downsk <12downks@gmail.com>
2016-02-01 18:38:19 -0600

    init commit
    Basic class structure

---
+++ src/drawingapplication/Ellipse.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package drawingapplication;
+
+/**
+ *
+ * @author 12dow_000
+ */
+public class Ellipse extends Shape {
+    double height;
+    double width;
+}
